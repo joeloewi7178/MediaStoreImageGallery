5e836143ceb894a4327314d848bbc3a72b74a93a
joeloewi7178 <joeloewi7178@gmail.com>
2022-01-06 18:17:09 +0900

    add: jvm target mod: downgrade kotlin gradle plugin (due to compose)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
     buildFeatures {
         compose = true
     }
